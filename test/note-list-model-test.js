cfdd982a8358a083db83b0c9b165f4e400b09478
Kynosaur <kye.bracey@gmail.com>
2017-06-20 10:21:16 +0100

    Adds tests for note list model

---
+++ test/note-list-model-test.js
@@ -0,0 +1,21 @@
+function testNoteListHasAnArrayToStoreNotes() {
+  var noteList = new NoteList;
+  assert.isTrue(noteList.notes === []);
+}
+
+function testNoteListCreatesNotes() {
+  var noteList = new NoteList;
+  noteList.addNewNote("test1");
+  assert.isTrue(noteList.notes.length === 1);
+}
+
+function testNoteListReturnsTheArrayOfNotes() {
+  var noteList = new NoteList;
+  noteList.addNewNote("test1");
+  noteList.addNewNote("test2");
+  assert.isTrue(noteList.displayNotes().length === 2);
+}
+
+testNoteListHasAnArrayToStoreNotes();
+testNoteListCreatesNotes();
+testNoteListReturnsTheArrayOfNotes();
